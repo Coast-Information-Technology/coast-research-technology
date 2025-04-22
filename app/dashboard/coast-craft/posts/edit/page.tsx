4d682e7e5fc433519d34518bb5124bf20f602bb8
Toluwalope-Coast <rodneytoluwalope@gmail.com>
2025-04-22 10:10:00 +0100

    👌: Created Craft Pages

---
+++ app/dashboard/coast-craft/posts/edit/page.tsx
@@ -0,0 +1,14 @@
+// /pages.tsx
+'use client';
+
+import React from 'react';
+
+const page = () => {
+  return (
+    <section>
+      <h1>Edit Post</h1>
+    </section>
+  );
+};
+
+export default page;
---
+++ app/dashboard/coast-craft/posts/page.tsx
@@ -0,0 +1,14 @@
+// /pages.tsx
+'use client';
+
+import React from 'react';
+
+const page = () => {
+  return (
+    <section>
+      <h1>View All Posts</h1>
+    </section>
+  );
+};
+
+export default page;
